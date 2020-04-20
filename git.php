2eca6dc694a8af9f931b8cdc589a9fe02cfe523d
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2020-04-20 04:06:15 -0700

    a

--- git.php
+++ git.php
@@ -1,4 +1,14 @@
 <?php
 
-//echo exec("git pull");
-print(exec("git pull https://git@github.com:peymanvalikhanli/task_api.git master"));
+echo "salam";
+// echo exec("git pull git@github.com:peymanvalikhanli/task_api.git master");
+
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+}
+// Print the exec output inside of a pre element
+print("<pre>" . execPrint("git pull") . "</pre>");
